df505d30eda04b59876f12f2c7f202ccb5818d84
Sara Stavang <stavangsara@gmail.com>
2026-03-09 14:22:46 +0100

    Added the code I wrote in Sara branch to main

---
+++ frontend/src/routes/Appointments/+page.ts
@@ -0,0 +1,19 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch }) => {
+	const response = await fetch('/api/appointment');
+
+	if (!response.ok) {
+		return {
+			appointments: [],
+			error: 'Failed to load appointments'
+		};
+	}
+
+	const appointments = await response.json();
+
+	return {
+		appointments,
+		error: null
+	};
+};
