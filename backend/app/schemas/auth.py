459bd7b37c095d1502a187b05af37b049cfee8ba
jathvl <230452033+jathvl@users.noreply.github.com>
2026-05-02 19:25:18 +0200

    Allow donor signup

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,6 +1,24 @@
 from pydantic import BaseModel
+from ..db.sqlc.models import BloodType
 
 
 class LoginRequestData(BaseModel):
     email: str
     password: str
+
+
+class RegisterDonorRequestData(BaseModel):
+    email: str
+    password: str
+
+    name: str
+    phone_number: str
+    street_name: str
+    street_number: str
+    apt_number: str | None
+    postal_code: str
+    city: str
+    country: str
+
+    preferred_bloodbank_id: int
+    blood_type: BloodType | None
